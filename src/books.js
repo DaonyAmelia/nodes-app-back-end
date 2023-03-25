24254978ad9035febcebf601b6b3d46b7a8c938a
DaonyAmelia <c282dsy0707@bangkit.academy>
2023-03-25 11:51:14 +0700

    fix bugs host value

--- src/books.js
+++ src/books.js
@@ -1,17 +1,17 @@
 const books = [
     {
-        "id":  string,
-        "name": string,
-        "year": number,
-        "author": string,
-        "summary": string,
-        "publisher": string,
-        "pageCount": number,
-        "readPage": number,
-        "finished": boolean,
-        "reading": boolean,
-        "insertedAt": string,
-        "updatedAt": string,
+    "id": "Qbax5Oy7L8WKf74l",
+    "name": "Buku A",
+    "year": 2010,
+    "author": "John Doe",
+    "summary": "Lorem ipsum dolor sit amet",
+    "publisher": "Dicoding Indonesia",
+    "pageCount": 100,
+    "readPage": 25,
+    "finished": false,
+    "reading": false,
+    "insertedAt": "2021-03-04T09:11:44.598Z",
+    "updatedAt": "2021-03-04T09:11:44.598Z"
     }
 ];
 
